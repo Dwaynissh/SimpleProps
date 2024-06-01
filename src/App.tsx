095a07d33963eff9c5d1f5e76c0478022dc42c3a
dwaynissh <dwaynissh@gmail.com>
2024-06-01 16:49:15 +0100

    commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import CardDisplay from './display/CardDislay';
+
+function App() {
+  return (
+    <>
+    <div style={{width: "100%", height: "100%"}}>
+       <CardDisplay/>
+       <div>Hello</div>
+    </div>
+   
+    </>
+  );
+}
+
+export default App;
